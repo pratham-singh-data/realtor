8ce89464ff5f9027836dc8f9424149daba943d71
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-07-24 14:44:31 +0530

    Security Improved

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async(url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '736d97bf73msh248275ae26aea05p15d319jsn0bcc867b3080',
+            'X-RapidAPI-Key': process.env.NEXT_APP_RAPID_API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     })
